8ce6e05f0fa93ab1bed0baf56dbf081d8c7bcdff
Epslin <60341743+jiadevr@users.noreply.github.com>
2023-11-05 10:46:45 +0800

    Initial commit

---
+++ Source/DataDriveLogic/DataDriveLogicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DataDriveLogicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATADRIVELOGIC_API ADataDriveLogicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
